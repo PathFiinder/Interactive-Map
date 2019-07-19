ab1747fc81e8e91486a1c123910ecbcd6fd48a11
Krystian Konecki <krystian_konecki@wp.pl>
2019-07-20 00:06:15 +0200

    Adding searching tab

--- js/Map.js
+++ js/Map.js
@@ -5,16 +5,20 @@ class Map {
     constructor() {
         this.searchCountryClass = document.querySelector('.countries__search');
         this.searchingTypeClass = document.querySelector('.coutries__searching-type');
+        this.findedCountriesClass = document.querySelector('.countries__finded');
         this.visitedCountries = ["PL"]
         this.listOfCountries = Object.values(jvmCountries)
         this.ifExist = false;
         this.index = 0;
+
+        this.deletedCountryFromListOfCountries = [];
         this.getContinent();
         this.initializeMap();
         this.searchCountry();
 
 
     }
+
 
     getContinent() {
         let listAllCountries = Object.values(allCountries)
@@ -27,29 +31,61 @@ class Map {
         })
     }
 
-    addVisitedCountry(code) {
+    addVisitedCountry() {
+        this.findedCountriesClass.addEventListener('click', (event) => {
+            if (event.target.dataset.id) {
+                if (this.visitedCountries.length != 0) {
+                    for (let i = 0; i < this.visitedCountries.length; i++) {
+                        if (this.visitedCountries[i] == event.target.dataset.id) {
+                            this.ifExist = true;
+                            this.index = i;
+                            break;
+                        } else this.ifExist = false
+                    }
+                } else {
+                    this.ifExist = false;
+                }
 
-        if (this.visitedCountries.length != 0) {
-            for (let i = 0; i < this.visitedCountries.length; i++) {
-                if (this.visitedCountries[i] == code) {
-                    this.ifExist = true;
-                    this.index = i;
-                    break;
-                } else this.ifExist = false
+                if (this.ifExist == false) {
+                    this.visitedCountries.push(event.target.dataset.id)
+                    document.querySelector('.header__counter-visited--number').textContent = this.visitedCountries.length;
+
+                    this.listOfCountries.forEach((element, index) => {
+                        if (element.code == event.target.dataset.id) {
+                            this.listOfCountries.splice(index, 1);
+                            this.deletedCountryFromListOfCountries.push(element)
+                        }
+                    })
+                    console.log(this.deletedCountryFromListOfCountries)
+
+
+
+
+
+                }
+
+
+
+
+                const map = $('.map-container__map').vectorMap('get', 'mapObject');
+                map.setSelectedRegions(event.target.dataset.id);
+                this.searchCountryClass.value = ""
+
+                let child = this.findedCountriesClass.lastElementChild;
+                while (child) {
+                    this.findedCountriesClass.removeChild(child);
+                    child = this.findedCountriesClass.lastElementChild;
+                }
+
+                //console.log(this.visitedCountries)
+                //console.log(this.listOfCountries)
             }
-        } else {
-            this.ifExist = false;
-        }
+        })
 
-        if (this.ifExist == false) {
-            this.visitedCountries.push(code)
-            document.querySelector('.header__counter-visited--number').textContent = this.visitedCountries.length;
-        } else if (this.ifExist == true) {
-            this.visitedCountries.splice(this.index, 1)
-            document.querySelector('.header__counter-visited--number').textContent = this.visitedCountries.length;
-        }
 
-        console.log(this.visitedCountries)
+
+
+
     }
 
 
@@ -72,10 +108,56 @@ class Map {
                     const regionName = map.getRegionName(string);
                     const code = string
 
-                    this.addVisitedCountry(code);
+                    if (this.visitedCountries.length != 0) {
+                        for (let i = 0; i < this.visitedCountries.length; i++) {
+                            if (this.visitedCountries[i] == code) {
+                                this.ifExist = true;
+                                this.index = i;
+                                break;
+                            } else this.ifExist = false
+                        }
+                    } else {
+                        this.ifExist = false;
+                    }
+
+                    if (this.ifExist == false) {
+                        this.visitedCountries.push(code)
+                        document.querySelector('.header__counter-visited--number').textContent = this.visitedCountries.length;
+                    } else if (this.ifExist == true) {
+                        this.visitedCountries.splice(this.index, 1)
+                        document.querySelector('.header__counter-visited--number').textContent = this.visitedCountries.length;
+
+                       
+                    }
+
+                    console.log(this.visitedCountries)
+                    
                 }
             });
         })
+    }
+
+    createElement(code, countryName) {
+        const divElement = document.createElement("div");
+        divElement.classList.add("single-country");
+        divElement.dataset.id = `${code}`;
+        this.findedCountriesClass.appendChild(divElement);
+
+        const imgElement = document.createElement("img");
+        imgElement.classList.add("single-country__flag");
+        imgElement.src = `./images/flag/${code.toLowerCase()}.png`;
+        imgElement.alt = `${code} flag`;
+        document.querySelector(`[data-id = "${code}"]`).appendChild(imgElement);
+
+        const countryNameElement = document.createElement("h3");
+        countryNameElement.classList.add("single-country__name");
+        countryNameElement.textContent = `${countryName}`;
+        document.querySelector(`[data-id = "${code}"]`).appendChild(countryNameElement);
+
+        const binElement = document.createElement("i")
+        binElement.classList.add('single-country__bin', 'far', 'fa-trash-alt');
+        binElement.dataset.idb = `${code}`
+        document.querySelector(`[data-id = "${code}"]`).appendChild(binElement);
     }
 
 
@@ -83,25 +165,24 @@ class Map {
         this.searchCountryClass.addEventListener('input', (event) => {
             event.preventDefault();
             const value = event.target.value;
-            //includes
-            this.listOfCountries.forEach((element) => {
-                let elementValue = element.code
 
+            let child = this.findedCountriesClass.lastElementChild;
+            while (child) {
+                this.findedCountriesClass.removeChild(child);
+                child = this.findedCountriesClass.lastElementChild;
+            }
+
+            this.listOfCountries.forEach((element) => {
                 if (this.searchingTypeClass.value == "code") {
-                    if (value.length == 2) {
-                        if (value.toLowerCase() == elementValue.toLowerCase()) {
-                            const map = $('.map-container__map').vectorMap('get', 'mapObject');
-                            map.setSelectedRegions(value.toUpperCase());
-                            this.addVisitedCountry(value.toUpperCase());
-                            this.searchCountryClass.value = ""
-                            console.log(this.visitedCountries)
-                        }
+                    if (element.code.toLowerCase().includes(value.toLowerCase())) {
+                        this.createElement(element.code, element.name)
+                        this.addVisitedCountry();
                     }
-                } else if(this.searchingTypeClass.value == "country-name"){
-                    if(element.name.toLowerCase().includes(value.toLowerCase())){
+                } else if (this.searchingTypeClass.value == "country-name") {
+                    if (element.name.toLowerCase().includes(value.toLowerCase())) {
                         console.log(element.name)
                     }
-                } 
+                }
 
 
 
