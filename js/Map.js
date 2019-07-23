6e396b6aeaa5b7c119dca3f3ecb2686bcbfaefef
Krystian Konecki <krystian_konecki@wp.pl>
2019-07-23 15:27:58 +0200

    Adding continents section

--- js/Map.js
+++ js/Map.js
@@ -8,22 +8,32 @@ class Map {
         this.findedCountriesClass = document.querySelector('.countries__finded');
         this.visitedCountryClass = document.querySelector('.visitedCountries');
         this.barOfAllCountries = document.querySelector('.instruction__countryList');
+        this.visitedContinentClass = document.querySelectorAll('.singleCountryContinent');
+        this.instructionButton = document.querySelector('.instruction__button');
         this.visitedCountries = [];
         this.listOfCountries = Object.values(jvmCountries)
         this.ifExist = false;
         this.index = 0;
-        //console.log(this.listOfCountries)
         this.listOfContinents = ['Asia', 'Africa', 'AmericaN', 'AmericaS', 'Europe', 'Oceania', 'Antarctica'];
+        this.showInstruction();
         this.getContinent();
         this.barListOfAllCountries();
         this.initializeMap();
         this.searchCountry();
         this.changeSelectValue();
+        this.deleteCountryBin();
 
     }
 
-    barListOfAllCountries(){
-        this.barOfAllCountries.addEventListener('click',() => {
+    showInstruction(){
+        this.instructionButton.addEventListener('click',() => {
+            document.querySelector('.instruction').classList.toggle('instruction--active')
+            document.querySelector('.instruction__content-container').classList.toggle('instruction__content-container--active');
+        })
+    }
+
+    barListOfAllCountries() {
+        this.barOfAllCountries.addEventListener('click', () => {
             this.listOfCountries.forEach(element => {
                 document.querySelector('.countryListContainer__countries').innerHTML += `${element.name} : ${element.code} <br>`
             })
@@ -34,10 +44,9 @@ class Map {
         document.querySelector('.countryListContainer__icon').addEventListener('click', () => {
             document.querySelector('.countryListContainer__countries').textContent = ""
             document.querySelector('.countryListContainer__icon').classList.remove('countryListContainer__icon--active')
-            document.querySelector('.countryListContainer').classList.remove('countryListContainer--active');     
-        })  
+            document.querySelector('.countryListContainer').classList.remove('countryListContainer--active');
+        })
     }
-
 
     getContinent() {
         let listAllCountries = Object.values(allCountries)
@@ -50,12 +59,12 @@ class Map {
         })
     }
 
-    clearChilds() {
+    clearChilds(className) {
 
-        let child = this.findedCountriesClass.lastElementChild;
+        let child = className.lastElementChild;
         while (child) {
-            this.findedCountriesClass.removeChild(child);
-            child = this.findedCountriesClass.lastElementChild;
+            className.removeChild(child);
+            child = className.lastElementChild;
         }
     }
 
@@ -83,14 +92,18 @@ class Map {
                             this.addVisitedCountryContinent();
                         }
                     })
-                    document.querySelector('.header__counter-visited--number').textContent = this.visitedCountries.length;
+                    document.querySelector('.header__counted-visited--number').textContent = this.visitedCountries.length;
                 }
 
                 const map = $('.map-container__map').vectorMap('get', 'mapObject');
                 map.setSelectedRegions(event.target.dataset.id);
                 this.searchCountryClass.value = ""
 
-                this.clearChilds();
+                this.clearChilds(this.findedCountriesClass);
+
+                document.querySelector('.countryListContainer__countries').textContent = ""
+                document.querySelector('.countryListContainer__icon').classList.remove('countryListContainer__icon--active')
+                document.querySelector('.countryListContainer').classList.remove('countryListContainer--active');
 
             }
         })
@@ -101,6 +114,30 @@ class Map {
 
     }
 
+    createCountryContinent(name, code, continent) {
+        const div = document.createElement("div");
+        div.classList.add("singleCountryContinent__container");
+        div.dataset.idcountry = `${code}`;
+        document.querySelector(`.visitedCountries__wrapper--${continent}`).appendChild(div);
+
+        const img = document.createElement("img");
+        img.classList.add("singleCountryContinent__flag");
+        img.src = `./images/flag/${code.toLowerCase()}.png`;
+        img.alt = `${code} flag`;
+        document.querySelector(`[data-idcountry = "${code}"]`).appendChild(img);
+
+        const countryName = document.createElement("h3");
+        countryName.classList.add("singleCountryContinent__name");
+        countryName.textContent = `${name}`;
+        document.querySelector(`[data-idcountry = "${code}"]`).appendChild(countryName);
+
+        const bin = document.createElement("i")
+        bin.classList.add('singleCountryContinent__bin', 'far', 'fa-trash-alt');
+        bin.dataset.idb = `${code}`
+        document.querySelector(`[data-idcountry = "${code}"]`).appendChild(bin);
+    }
+
+
     addVisitedCountryContinent() {
         if (this.visitedCountries != 0) {
             this.visitedCountryClass.classList.add('visitedCountries--active')
@@ -108,6 +145,7 @@ class Map {
                 for (let i = 0; i < this.listOfContinents.length; i++) {
                     if (this.listOfContinents[i] == element.continent) {
                         document.querySelector(`.visitedCountries__continent--${element.continent}`).classList.add('visitedCountries__continent--active');
+                        this.createCountryContinent(element.name, element.code, element.continent)
                         break;
                     }
                 }
@@ -124,16 +162,16 @@ class Map {
 
             this.listOfContinents.forEach(element => {
                 let counter = 0;
-                for(let i=0;i<this.visitedCountries.length;i++){
-                    if(this.visitedCountries[i].continent == element)
-                    counter++;
+                for (let i = 0; i < this.visitedCountries.length; i++) {
+                    if (this.visitedCountries[i].continent == element)
+                        counter++;
                 }
 
-                if(counter == 0){
+                if (counter == 0) {
                     document.querySelector(`.visitedCountries__continent--${element}`).classList.remove('visitedCountries__continent--active');
                 }
             })
-           
+
 
 
         } else {
@@ -184,13 +222,23 @@ class Map {
                                 this.listOfCountries.splice(index, 1);
                             }
                         })
-                        document.querySelector('.header__counter-visited--number').textContent = this.visitedCountries.length;
+                        document.querySelector('.header__counted-visited--number').textContent = this.visitedCountries.length;
+
+                        this.visitedContinentClass.forEach(element => {
+                            let child = element.lastElementChild;
+                            while (child) {
+                                element.removeChild(child);
+                                child = element.lastElementChild;
+                            }
+                        })
+
                         this.addVisitedCountryContinent();
                     } else if (this.ifExist == true) {
                         this.listOfCountries.push(this.visitedCountries[this.index]);
+                        document.querySelector(`[data-idcountry = "${this.visitedCountries[this.index].code}"]`).remove();
                         this.visitedCountries.splice(this.index, 1);
                         this.checkIfCountryContinentExist();
-                        document.querySelector('.header__counter-visited--number').textContent = this.visitedCountries.length;
+                        document.querySelector('.header__counted-visited--number').textContent = this.visitedCountries.length;
                     }
                 }
             });
@@ -214,16 +262,12 @@ class Map {
         countryNameElement.textContent = `${countryName}`;
         document.querySelector(`[data-id = "${code}"]`).appendChild(countryNameElement);
 
-        /*const binElement = document.createElement("i")
-        binElement.classList.add('single-country__bin', 'far', 'fa-trash-alt');
-        binElement.dataset.idb = `${code}`
-        document.querySelector(`[data-id = "${code}"]`).appendChild(binElement);*/
     }
 
     changeSelectValue() {
         document.querySelector('.coutries__searching-type').addEventListener('change', () => {
             this.searchCountryClass.value = ""
-            this.clearChilds();
+            this.clearChilds(this.findedCountriesClass);
         })
     }
 
@@ -231,24 +275,64 @@ class Map {
         this.searchCountryClass.addEventListener('input', (event) => {
             event.preventDefault();
             const value = event.target.value;
-            this.clearChilds();
+            this.clearChilds(this.findedCountriesClass);
+
+            this.visitedContinentClass.forEach(element => {
+                let child = element.lastElementChild;
+                while (child) {
+                    element.removeChild(child);
+                    child = element.lastElementChild;
+                }
+            })
+            
 
             this.listOfCountries.forEach((element) => {
                 if (this.searchingTypeClass.value == "code") {
                     if (element.code.toLowerCase().includes(value.toLowerCase())) {
                         this.createElementCountries(element.code, element.name)
                         this.addVisitedCountry();
+                        this.visitedCountryClass.classList.remove('visitedCountries--active')
                     }
                 } else if (this.searchingTypeClass.value == "country-name") {
                     if (element.name.toLowerCase().includes(value.toLowerCase())) {
                         this.createElementCountries(element.code, element.name)
                         this.addVisitedCountry();
+                        this.visitedCountryClass.classList.remove('visitedCountries--active')
                     }
                 }
             })
 
             if (this.searchCountryClass.value == "") {
-                this.clearChilds();
+                this.clearChilds(this.findedCountriesClass);
+                this.addVisitedCountryContinent();
+            }
+        })
+
+    }
+
+    deleteCountryBin() {
+        document.querySelector('.visitedCountries__container').addEventListener('click', event => {
+            if (event.target.dataset.idb) {
+                const code = event.target.dataset.idb
+                let id = 0;
+
+                this.visitedCountries.forEach((element,index) => {
+                    if(element.code == code){
+                        id = index;
+                    }
+                })
+
+                this.listOfCountries.push(this.visitedCountries[id]);
+                document.querySelector(`.visitedCountries__wrapper--${this.visitedCountries[id].continent}`).removeChild(document.querySelector(`[data-idcountry = "${this.visitedCountries[id].code}"]`))
+                this.visitedCountries.splice(id, 1);
+                this.checkIfCountryContinentExist();
+                document.querySelector('.header__counted-visited--number').textContent = this.visitedCountries.length;
+                
+                const map = $('.map-container__map').vectorMap('get', 'mapObject');
+                map.clearSelectedRegions();
+                this.visitedCountries.forEach(element => {
+                    map.setSelectedRegions(element.code)
+                }) 
             }
         })
 
